833199eab7a71f5b95fdb6ee829717139739da03
Ivan Li <ivankyli@gmail.com>
2015-07-09 16:29:39 +1200

    Forcing not to reboot. Seems to improve stability.

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,7 @@ include_recipe 'boxstarter::default'
 
 boxstarter "boxstarter run" do
   password node['login_password']
-  disable_reboots false
+  disable_reboots true
   
   code <<-EOH
     Set-TaskbarOptions -Lock -Size Small -Dock Bottom
